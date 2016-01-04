296b7abef7107f99ff6392776ec3b70d83d19141
leeyoseob <iosid89@gmail.com>
2016-01-04 16:00:02 +0900

    change fileIO Model, prepare body data logic parse and store

--- Trevi/Http/PreparedData.swift
+++ Trevi/Http/PreparedData.swift
@@ -25,20 +25,53 @@ class PreparedData {
     private var req : Request?
 
 
+    var filemanager  = File()
+    
     func appendReadData(params : ReceivedParams) -> (Int,Int){
         
         let (strData,_) = String.fromCStringRepairingIllFormedUTF8(params.buffer)
         let data = strData! as String
         var headerLength = 0;
         print(data)
+        print("end")
         //header
         if data.containsString("HTTP/1."){
             req = nil
             req = setupRequest(data)
+
+            filemanager.createFile("\("123")")
             if let contentLength = req?.header[Content_Length]{
                 content_length =  Int(contentLength)!
                 headerLength = params.length
             }
+            /*
+            
+            if req!.header[Content_Length] != nil && req!.bodyFragments.count == 0  {
+            let headerList = req!.headerString.componentsSeparatedByString(CRLF)
+            
+            var buff = String()
+            var flag = false
+            
+            for line in headerList{
+            if line.length() == 0 {
+            flag = true
+            }
+            if flag && line.length() > 0{
+            buff += line
+            buff += CRLF
+            }
+            }
+            
+            if buff.length() > 0 {
+            req!.bodyFragments.insert(buff, atIndex: 0)
+            }
+            }
+
+            
+            
+            
+            */
+            
         }else{
             //read file
             //req.body
@@ -52,8 +85,12 @@ class PreparedData {
             
             */
 //            req?.body.appendBytes(params.buffer, length: params.length)
+            
+            filemanager.write("test.txt", data: data, encoding: NSUTF8StringEncoding)
+//            try! File.writetest( "image.txt", data:data, encoding: NSUTF8StringEncoding )
             req?.bodyFragments.append(data)
         }
+    
         return (content_length,headerLength)
     }
     
--- Trevi/Utility/File.swift
+++ Trevi/Utility/File.swift
@@ -12,8 +12,57 @@ public enum FileError: ErrorType {
     case FileOpenError( msg:String )
 }
 
-class File {
+public class File {
 
+    
+    let filemgr = NSFileManager.defaultManager()
+    
+    public func createFile(path : String){
+        filemgr.createFileAtPath(path, contents: nil, attributes: nil)
+    }
+    
+    public func existsFile(path : String){
+    
+        if filemgr.fileExistsAtPath(path) {
+            print("File exists")
+        } else {
+            print("File not found")
+        }
+    }
+    func removeFile(path:String){
+        do {
+            try filemgr.removeItemAtPath(path)
+        } catch {
+            let ioError = error as NSError
+            FileError.FileOpenError ( msg: ioError.localizedDescription )
+        }
+    }
+    
+    func write(filePath: String, data: String, encoding: UInt = NSUTF8StringEncoding ){
+        let file: NSFileHandle? = NSFileHandle(forUpdatingAtPath: filePath)
+        if file != nil {
+           writeImpliment(file!, data: data, encoding: encoding)
+        } else {
+            createFile(filePath)
+            writeImpliment(NSFileHandle(forUpdatingAtPath: filePath)!, data: data, encoding: encoding)
+        }
+    }
+    
+    func read(filePath: String, encoding: UInt = NSUTF8StringEncoding ) -> String {
+        let file: NSFileHandle? = NSFileHandle(forReadingAtPath: filePath)
+        return String(data: (file?.readDataToEndOfFile())!, encoding: encoding)!
+    }
+    
+    private func writeImpliment(file: NSFileHandle, data: String, encoding: UInt = NSUTF8StringEncoding ){
+        file.seekToEndOfFile()
+        let data = (data as NSString).dataUsingEncoding(encoding)
+        file.writeData(data!)
+        file.closeFile()
+    }
+    
+    func closeFile(){
+        
+    }
     // Reading
     static func read(filePath: String, encoding: UInt = NSUTF8StringEncoding ) throws -> String {
         do {
@@ -34,5 +83,7 @@ class File {
             throw FileError.FileOpenError ( msg: ioError.localizedDescription )
         }
     }
+    
+    
 
 }
