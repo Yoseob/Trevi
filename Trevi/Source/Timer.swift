bfe1a67211fdd41e50be3a1cc7ad24712bef967d
JangTaeHwan <tata8664@gmail.com>
2016-02-21 18:30:59 +0900

    Add Libuv Timer module

---
+++ Trevi/Source/Timer.swift
@@ -0,0 +1,53 @@
+//
+//  Timer.swift
+//  Trevi
+//
+//  Created by JangTaehwan on 2016. 2. 21..
+//  Copyright © 2016년 LeeYoseob. All rights reserved.
+//
+
+import Libuv
+
+public class Timer : Handle {
+    
+    public let timerhandle : uv_timer_ptr
+    
+    public init() {
+        self.timerhandle = uv_timer_ptr.alloc(1)
+        uv_timer_init(uv_default_loop(), self.timerhandle)
+        
+        super.init(handle: uv_handle_ptr(self.timerhandle))
+    }
+}
+
+
+// Timer static functions.
+
+extension Timer {
+    
+    public static func start(handle : uv_timer_ptr, timeout : UInt64,  count : UInt64 = 1) {
+        uv_timer_start(handle, Timer.onTimeout , timeout, count)
+    }
+    
+    public static func stop(handle : uv_timer_ptr) {
+        uv_timer_stop(handle)
+    }
+    
+    public static func again(handle : uv_timer_ptr) {
+        uv_timer_again(handle)
+    }
+    
+    public static func setRepeat(handle : uv_timer_ptr, count : UInt64) {
+        uv_timer_set_repeat(handle, count)
+    }
+}
+
+
+// Timer static callbacks.
+
+extension Timer {
+    
+    public static var onTimeout : uv_timer_cb = { (handle) in
+        
+    }
+}
