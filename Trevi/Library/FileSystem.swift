74ea72d3f41780d9824e0378fc1c0a9bcfaa4137
JangTaeHwan <tata8664@gmail.com>
2016-03-10 04:01:17 +0900

    Modify File stream run loop and request close

--- Trevi/Library/FileSystem.swift
+++ Trevi/Library/FileSystem.swift
@@ -11,181 +11,186 @@ import Foundation
 
 
 public class FileSystem {
-
-public struct Options {
-    public var fd : Int32! = nil
-    public var flags : Int32! = nil
-    public var mode : Int32! = nil
-}
+    
+    public struct Options {
+        public var fd : Int32! = nil
+        public var flags : Int32! = nil
+        public var mode : Int32! = nil
+    }
     
     
-public static func close(handle : uv_handle_ptr) {
-    
-    let info = UnsafeMutablePointer<FSInfo>(handle.memory.data)
-    let request = info.memory.request
-    let loop = info.memory.loop
-    
-    Handle.close(handle)
-    FSBase.close(loop, request: request)
-    Loop.close(loop)
-    request.dealloc(1)
-    info.dealloc(1)
-}
-    
-    
-// Should be inherited from StreamReadable
-
-public class ReadStream {
-    
-    public let loop : Loop
-    public let pipe : Pipe
-    public var options : Options = Options()
-    
-    public init(path : String, options : Options? = nil) {
+    public static func close(handle : uv_handle_ptr) {
         
-        self.loop = Loop()
-        self.pipe = Pipe(loop: loop.loopHandle)
-        self.options.flags = O_RDONLY
-        self.options.mode = 0o666
+        let info = UnsafeMutablePointer<FSInfo>(handle.memory.data)
+        let request = info.memory.request
+        let loop = info.memory.loop
         
-        if let options = options { self.setOptions(options) }
+        Handle.close(handle)
+        FSBase.close(loop, request: request)
+        info.dealloc(1)
+    }
+    
+    
+    // Should be inherited from StreamReadable
+    
+    public class ReadStream {
         
+        public let loop : Loop
+        public let pipe : Pipe
+        public var options : Options = Options()
         
-        if self.options.fd == nil {
-            self.options.fd = FSBase.open(self.loop.loopHandle, handle: self.pipe.pipeHandle, path : path,
-                flags: self.options.flags, mode: self.options.mode)
-        }
-        
-        if self.options.fd <= 0 {
-            // Should handle error
+        public init(path : String, options : Options? = nil) {
             
-            print("Read Stream file open error \(self.options.fd)")
+            self.loop = Loop()
+            self.pipe = Pipe(loop: loop.loopHandle)
+            self.options.flags = O_RDONLY
+            self.options.mode = 0o666
+            
+            if let options = options { self.setOptions(options) }
+            
+            
+            if self.options.fd == nil {
+                self.options.fd = FSBase.open(self.loop.loopHandle, handle: self.pipe.pipeHandle, path : path,
+                    flags: self.options.flags, mode: self.options.mode)
+            }
+            
+            if self.options.fd <= 0 {
+                // Should handle error
+                
+                print("Read Stream file open error \(self.options.fd)")
+            }
+            else{
+                Pipe.open(self.pipe.pipeHandle, fd: self.options.fd)
+                
+                self.pipe.event.onClose = { (handle) in
+                    
+                    FileSystem.close(handle)
+                }
+            }
         }
-        else{
-            Pipe.open(self.pipe.pipeHandle, fd: self.options.fd)
+        
+        deinit{
+            Handle.close(self.pipe.handle)
+            Loop.close(self.loop.loopHandle)
+        }
+        
+        
+        func setOptions(options : Options) {
+            self.options.fd = options.fd
+            self.options.flags = options.flags == nil ? O_RDONLY : options.flags
+            self.options.mode = options.mode == nil ?  0o666 : options.mode
+        }
+        
+        
+        public func onClose(callback : ((handle : uv_handle_ptr)->Void)) {
             
             self.pipe.event.onClose = { (handle) in
                 
+                callback(handle: handle)
                 FileSystem.close(handle)
             }
         }
-    }
-    deinit{
-        Handle.close(self.pipe.handle)
-    }
-    
-    func setOptions(options : Options) {
-        self.options.fd = options.fd
-        self.options.flags = options.flags == nil ? O_RDONLY : options.flags
-        self.options.mode = options.mode == nil ?  0o666 : options.mode
-    }
-    
-    public func onClose(callback : ((handle : uv_handle_ptr)->Void)) {
         
-        self.pipe.event.onClose = { (handle) in
         
-            callback(handle: handle)
-            FileSystem.close(handle)
-        }
-    }
-    
-    
-    public func readStart(callback : ((error : Int32, data : NSData)->Void)) {
-        
-        self.pipe.event.onRead = { (handle, data) in
+        public func readStart(callback : ((error : Int32, data : NSData)->Void)) {
             
-            let info = UnsafeMutablePointer<FSInfo>(handle.memory.data)
-            info.memory.toRead = info.memory.toRead - UInt64(data.length)
-            
-            callback(error : 0, data : data)
-            
-            if info.memory.toRead <= 0 {
-                Handle.close(uv_handle_ptr(handle))
-            }
-        }
-        
-        Stream.readStart(self.pipe.streamHandle)
-    }
-    
-    
-    public func pipeStream(writeStream : WriteStream) {
-        
-        self.onClose() {
-            handle in
-            Handle.close(writeStream.pipe.handle)
-        }
-        
-        self.readStart() {
-            (error, data) in
-            writeStream.writeData(data)
-        }
-        
-        Loop.run(self.loop.loopHandle, mode: UV_RUN_ONCE)
-        Loop.run(writeStream.loop.loopHandle, mode: UV_RUN_ONCE)
-    }
-    
-}
-    
-    
-// Should be inherited from StreamReadable
-    
-public class WriteStream {
-    
-    public let loop : Loop
-    public let pipe : Pipe 
-    public var options : Options = Options()
-    
-    public init(path : String, options : Options? = nil) {
-        
-        self.loop = Loop()
-        self.pipe = Pipe(loop: loop.loopHandle)
-        self.options.flags = O_CREAT | O_WRONLY
-        self.options.mode = 0o666
-        
-        if let options = options { self.setOptions(options) }
-        
-        if self.options.fd == nil {
-            self.options.fd = FSBase.open(self.loop.loopHandle, handle: self.pipe.pipeHandle, path : path,
-                flags: self.options.flags, mode: self.options.mode)
-        }
-        
-        if self.options.fd <= 0 {
-            // Should handle error
-            
-            print("Write Stream file open error \(self.options.fd)")
-        }
-        else{
-            
-            Pipe.open(self.pipe.pipeHandle, fd: self.options.fd)
-            
-            self.pipe.event.onClose = { (handle) in
+            self.pipe.event.onRead = { (handle, data) in
                 
-                FileSystem.close(handle)
+                let info = UnsafeMutablePointer<FSInfo>(handle.memory.data)
+                info.memory.toRead = info.memory.toRead - UInt64(data.length)
+                
+                callback(error : 0, data : data)
+                
+                if info.memory.toRead <= 0 {
+                    Handle.close(uv_handle_ptr(handle))
+                }
+            }
+            
+            Stream.readStart(self.pipe.streamHandle)
+            Loop.run(self.loop.loopHandle, mode: UV_RUN_DEFAULT)
+        }
+        
+        
+        public func pipeStream(writeStream : WriteStream) {
+            
+            self.onClose() { (handle) in
+                
+                Handle.close(writeStream.pipe.handle)
+            }
+            
+            self.readStart() { (error, data) in
+                
+                writeStream.writeData(data)
+            }
+            
+            Loop.run(writeStream.loop.loopHandle, mode: UV_RUN_DEFAULT)
+        }
+        
+    }
+    
+    
+    // Should be inherited from StreamReadable
+    
+    public class WriteStream {
+        
+        public let loop : Loop
+        public let pipe : Pipe
+        public var options : Options = Options()
+        
+        public init(path : String, options : Options? = nil) {
+            
+            self.loop = Loop()
+            self.pipe = Pipe(loop: loop.loopHandle)
+            self.options.flags = O_CREAT | O_WRONLY
+            self.options.mode = 0o666
+            
+            if let options = options { self.setOptions(options) }
+            
+            if self.options.fd == nil {
+                self.options.fd = FSBase.open(self.loop.loopHandle, handle: self.pipe.pipeHandle, path : path,
+                    flags: self.options.flags, mode: self.options.mode)
+            }
+            
+            if self.options.fd <= 0 {
+                // Should handle error
+                
+                print("Write Stream file open error \(self.options.fd)")
+            }
+            else{
+                
+                Pipe.open(self.pipe.pipeHandle, fd: self.options.fd)
+                
+                self.pipe.event.onClose = { (handle) in
+                    
+                    FileSystem.close(handle)
+                }
             }
         }
-    }
-    deinit {
-        Handle.close(self.pipe.handle)
-    }
-    
-    
-    func setOptions(options : Options) {
-        self.options.fd = options.fd
-        self.options.flags = options.flags == nil ? O_CREAT | O_WRONLY : options.flags
-        self.options.mode = options.mode == nil ?  0o666 : options.mode
-    }
-    
-    
-    public func close() {
-        FileSystem.close(self.pipe.handle)
-    }
-    
-    
-    public func writeData(data : NSData) {
         
-        Stream.doWrite(data, handle: self.pipe.streamHandle)
+        deinit {
+            Handle.close(self.pipe.handle)
+            Loop.close(self.loop.loopHandle)
+        }
+        
+        
+        func setOptions(options : Options) {
+            self.options.fd = options.fd
+            self.options.flags = options.flags == nil ? O_CREAT | O_WRONLY : options.flags
+            self.options.mode = options.mode == nil ?  0o666 : options.mode
+        }
+        
+        
+        public func close() {
+            
+            FileSystem.close(self.pipe.handle)
+        }
+        
+        
+        public func writeData(data : NSData) {
+            
+            Stream.doWrite(data, handle: self.pipe.streamHandle)
+        }
+        
     }
     
-}
-
 }
